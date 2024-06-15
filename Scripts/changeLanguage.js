26aba000b6184be209b17f79d02fa4ba8a09375c
amaksix <amaksix@gmail.com>
2024-06-16 00:59:33 +0300

    main menu done, but translation are not positioned correctly

--- Scripts/changeLanguage.js
+++ Scripts/changeLanguage.js
@@ -3,6 +3,10 @@ function changeLanguage(lang) {
       .then(response => response.json())
       .then(data => {
         document.getElementById('selected-language-text').innerText = data.language;
+        document.getElementById('about').innerText = data.about;
+        document.getElementById('contact').innerText = data.contact;
+        document.getElementById('services').innerText = data.services;
+        document.getElementById('projects').innerText = data.projects;
       });
   }
 
