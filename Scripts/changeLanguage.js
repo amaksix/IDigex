3ece0901d59228245a1158c1c9083e5fb8d00e97
mteQbit <104194906+mteQbit@users.noreply.github.com>
2024-09-29 03:03:54 +0300

    some edits to the translation

--- Scripts/changeLanguage.js
+++ Scripts/changeLanguage.js
@@ -4,10 +4,18 @@ function changeLanguage(lang) {
       .then(data => {
         document.getElementById('selected-language-text').innerText = data.language;
         document.getElementById('about').innerText = data.about;
+        document.getElementById('about-button').innerText = data.about;
         document.getElementById('contact').innerText = data.contact;
-        document.getElementById('services').innerText = data.services;
-        document.getElementById('projects').innerText = data.projects;
+        document.getElementById('contact-button').innerText = data.contact;
+        document.getElementById('services').innerText = data.ourServices;
+        document.getElementById('services-button').innerText = data.services;
+        //document.getElementById('projects').innerText = data.projects;
+        document.getElementById('projects-button').innerText = data.projects;
         document.getElementById('descriptionText').innerText = data.descriptionText;
+        document.getElementById('about-text-first').innerText = data.aboutTextFirst;
+        document.getElementById('about-text-second').innerText = data.aboutTextSecond;
+        document.getElementById('websites-header').innerText = data.websitesHeader;
+        document.getElementById('websites-text').innerText = data.websitesText;
       });
   }
 
