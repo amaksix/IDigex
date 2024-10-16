939f1b603ba6079156897572850cfca549395902
mteQbit <104194906+mteQbit@users.noreply.github.com>
2024-10-16 16:17:01 +0300

    changes in loader

--- Scripts/loader.js
+++ Scripts/loader.js
@@ -2,7 +2,7 @@ window.addEventListener("load", () => {
     const loader = document.querySelector(".loader");
   
     loader.classList.add("loader--hidden");
-    console.log(loader);
+   
     loader.addEventListener("transitionend", () => {
       document.body.removeChild(loader);
     });
